acd6dffaf0d4ca3cc66398372f7f1ab652bb815f
Vikas-Prince <vikasprince30809@gmail.com>
2025-04-24 11:35:44 +0530

    Modified testcase

--- test/app.test.js
+++ test/app.test.js
@@ -65,7 +65,7 @@ describe("Conditional Rendering Logic", () => {
 
 describe("Toggle State Logic", () => {
   it("should toggle the value between true and false", () => {
-    let isToggled = false;
+    let isToggled = true;
 
     function toggle() {
       isToggled = !isToggled;
